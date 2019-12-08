7a5e934527b9d6d9cf7f97de0e667ca314086e9b
Edilma Riano <edilmaria@hotmail.com>
2019-12-08 13:59:01 -0500

    Add categroy controller, view and navigation links.  Program runs and executes properly

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -2,7 +2,6 @@
 {
     public class Cheese
     {
-        // just checking that github is working 
         public string Name { get; set; }
         public string Description { get; set; }
         public CheeseType Type { get; set; }
