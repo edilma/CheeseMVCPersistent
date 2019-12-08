1bb6ba1fc619e6cb5c25fed938fb03b142628f09
Edilma Riano <edilmaria@hotmail.com>
2019-12-08 09:48:52 -0500

    just checking that github is working

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -2,6 +2,7 @@
 {
     public class Cheese
     {
+        // just checking that github is working 
         public string Name { get; set; }
         public string Description { get; set; }
         public CheeseType Type { get; set; }
